449affe37a44471cfca6ac309a5ab4874ffd99f0
Ryan Fessler <rf841124@ohio.edu>
2026-09-02 18:51:38 -0400

    git-lab-program complete

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ryan Fessler
+ *        Date: 9/2/2026
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
